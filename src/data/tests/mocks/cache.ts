fd0be7a9031ce36d25acabe3398c6d0fb3edb73d
Matheus Torres <torresmiranda.rj@gmail.com>
2023-01-17 11:34:34 -0300

    feat: add activities array prop to ease calls ordering

--- src/data/tests/mocks/cache.ts
+++ src/data/tests/mocks/cache.ts
@@ -2,38 +2,51 @@ import { CacheStore } from '../../protocols/cache';
 import { SavePurchases } from '@/domain/useCases';
 
 export class CacheStoreSpy implements CacheStore {
+  activities: CacheStoreSpy.Activity[];
+
   deleteKey: string;
   insertKey: string;
-  deleteCallsCount: number = 0;
-  insertCallsCount: number = 0;
 
   insertValues: Array<SavePurchases.Params>;
 
   constructor() {
+    this.activities = [];
     this.insertValues = [];
   }
 
   public delete(key: string): void {
-    this.deleteCallsCount++;
+    this.activities.push(CacheStoreSpy.Activity.DELETE);
+    
     this.deleteKey = key;
   }
 
   public insert(key: string, value: any): void {
-    this.insertCallsCount++;
+    this.activities.push(CacheStoreSpy.Activity.INSERT);
+    
     this.insertKey = key;
-
     this.insertValues = value;
   };
 
   public $simulateDeleteError(): void {
     jest.spyOn(CacheStoreSpy.prototype, 'delete').mockImplementationOnce((): never => {
+      this.activities.push(CacheStoreSpy.Activity.DELETE);
+
       throw new Error();
     });
   }
 
   public $simulateInsertError(): void {
     jest.spyOn(CacheStoreSpy.prototype, 'insert').mockImplementationOnce((): never => {
+      this.activities.push(CacheStoreSpy.Activity.INSERT);
+
       throw new Error();
     });
+  }
+}
+
+export namespace CacheStoreSpy {
+  export enum Activity {
+    DELETE = 'delete',
+    INSERT = 'insert'
   }
 }
