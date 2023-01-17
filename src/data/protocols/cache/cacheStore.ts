65382b0267e6cbb3121cf88c37b46bfeeac8556e
Matheus Torres <torresmiranda.rj@gmail.com>
2023-01-16 21:19:51 -0300

    feat: add second param to insert method

--- src/data/protocols/cache/cacheStore.ts
+++ src/data/protocols/cache/cacheStore.ts
@@ -1,4 +1,8 @@
 export interface CacheStore {
   delete: (key: string) => void;
-  insert: (key: string) => void;
+  /**
+   * As CacheStore is a generic component, that can be used by any use case,
+   * The 2nd param of the insert method cannot receive a fixed type
+   */
+  insert: (key: string, value: any) => void;
 }
