3be8eb0b7a0775136735686731074ce75a7a83c7
Matheus Torres <torresmiranda.rj@gmail.com>
2023-01-17 18:02:08 -0300

    feat: add replace method
    it will simplify cache insertion

--- src/data/protocols/cache/cacheStore.ts
+++ src/data/protocols/cache/cacheStore.ts
@@ -2,7 +2,8 @@ export interface CacheStore {
   delete: (key: string) => void;
   /**
    * As CacheStore is a generic component, that can be used by any use case,
-   * The 2nd param of the insert method cannot receive a fixed type
+   * the 2nd parameter cannot receive a fixed type
    */
   insert: (key: string, value: any) => void;
+  replace: (key: string, value: any) => void;
 }
