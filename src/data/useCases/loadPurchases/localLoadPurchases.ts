c637ca8d85f5866b2bed573f097f1228456c855d
Matheus Torres <torresmiranda.rj@gmail.com>
2023-01-18 13:45:11 -0300

    feat: declare private key && create loadAll method

--- src/data/useCases/loadPurchases/localLoadPurchases.ts
+++ src/data/useCases/loadPurchases/localLoadPurchases.ts
@@ -2,17 +2,25 @@ import { CacheStore } from '@/data/protocols/cache';
 import { SavePurchases } from '@/domain/useCases';
 
 export class LocalLoadPurchases implements SavePurchases {
+  private readonly key: string;
+
   constructor(
     private readonly cacheStore: CacheStore, 
     private readonly timestamp: Date
-  ) {}
+  ) {
+    this.key = 'purchases';
+  }
+
+  public async loadAll(): Promise<void> {
+    this.cacheStore.fetch(this.key);
+  }
 
   public async save(purchases: Array<SavePurchases.Params>): Promise<void> {
     /**
      * Since the flow of cache insertion is always "delete old data -> insert new data", 
      * it is worth creating a method like replace 
      */
-    this.cacheStore.replace('purchases', {
+    this.cacheStore.replace(this.key, {
       timestamp: this.timestamp,
       value: purchases
     });
