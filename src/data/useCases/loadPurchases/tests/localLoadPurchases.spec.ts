b781dab02f143e21ff2b7dbcfc838bbf863805ba
Matheus Torres <torresmiranda.rj@gmail.com>
2023-01-18 13:33:17 -0300

    tests: ensure LocalLoadPurchases won't delete cache upon creation

---
+++ src/data/useCases/loadPurchases/tests/localLoadPurchases.spec.ts
@@ -0,0 +1,25 @@
+import { CacheStoreSpy, mockPurchases } from '@/data/tests';
+import { LocalLoadPurchases } from '@/data/useCases';
+
+type SUTTypes = {
+  cacheStore: CacheStoreSpy;
+  sut: LocalLoadPurchases;
+}
+
+const makeSUTFactory = (timestamp: Date = new Date()): SUTTypes => {
+  const cacheStore = new CacheStoreSpy();
+  const sut = new LocalLoadPurchases(cacheStore, timestamp);
+
+  return {
+    cacheStore,
+    sut
+  }
+}
+
+describe('LocalLoadPurchases', (): void => {
+  it('should not delete or insert cache during SUT.init', (): void => {
+    const factory: SUTTypes = makeSUTFactory();
+
+    expect(factory.cacheStore.activities).toEqual([]);
+  });
+});
