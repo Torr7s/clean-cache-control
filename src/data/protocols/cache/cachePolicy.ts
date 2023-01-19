a314bffb5f188549a4b897a70c4f242a2c66c8ae
Matheus Torres <torresmiranda.rj@gmail.com>
2023-01-19 11:04:58 -0300

    refactor: change private prop name

--- src/data/protocols/cache/cachePolicy.ts
+++ src/data/protocols/cache/cachePolicy.ts
@@ -1,12 +1,12 @@
 export class CachePolicy {
-  private static MAX_AGE_IN_DAYS: number = 3;
+  private static CACHE_MAX_AGE_IN_DAYS: number = 3;
 
   private constructor() {}
 
   public static validate(timestamp: Date, date: Date): boolean {
     const cacheMaxAge = new Date(timestamp);
 
-    cacheMaxAge.setDate(cacheMaxAge.getDate() + CachePolicy.MAX_AGE_IN_DAYS);
+    cacheMaxAge.setDate(cacheMaxAge.getDate() + CachePolicy.CACHE_MAX_AGE_IN_DAYS);
 
     return cacheMaxAge > date;
   }
